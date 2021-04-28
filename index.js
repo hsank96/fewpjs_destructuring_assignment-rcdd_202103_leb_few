76cb7dfc9206cf6e4c5f695c127944327c7fb419
Hazem El Sankari <hazemleb96@gmail.com>
2021-04-28 09:10:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const [, , , , , indg , ] = colors;
 // };
 
 
-const [muppetName, color, song, job, partner] = muppet
+const {muppetName, color, song, job, partner} = muppet
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 
